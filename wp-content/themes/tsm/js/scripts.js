09d63f5ba9341d4a22d4c4387e832fb754e535c4
Matt <matt@mspacecreative.com>
2018-11-22 12:15:16 -0400

    c

--- wp-content/themes/tsm/js/scripts.js
+++ wp-content/themes/tsm/js/scripts.js
@@ -1,23 +1,9 @@
 $(document).ready(function () {
 	$('#sidebar .menu li.menu-item-has-children').prepend('<span class="sub-toggle"><i class="fa fa-angle-down"></i></span>');
-	$('span.sub-toggle').click(function () {
+	$('span.sub-toggle').on('click', function () {
 		$(this).siblings('ul').slideToggle();
 		$(this).children().toggleClass('fa-angle-down fa-angle-up');
 	});
-	
-	$('span.sub-toggle').on('touchstart', function() {
-	    documentClick = true;
-	});
-	$('span.sub-toggle').on('touchmove', function() {
-	    documentClick = false;
-	});
-	$('span.sub-toggle').on('click touchend', function(event) {
-	    if (event.type == "click") documentClick = true;
-	    if (documentClick){
-	        $(this).siblings('ul').slideToggle();
-	        $(this).children().toggleClass('fa-angle-down fa-angle-up');
-	    }
-	 });
 	
 	$('.hero-slider, .solution_carousel').slick({
 	    //autoplay: true,
